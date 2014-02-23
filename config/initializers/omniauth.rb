b394385d1cbcf9d3355f961da00e7b8716b70dcd
RainMonster <the.rain.monster@gmail.com>
2014-02-22 23:33:04 -0800

    initializers for devise and omniauth which don't repeat the Facebook callback

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
-  :scope => 'basic_info,user_photos'
+  :scope => 'basic_info,email,user_photos'
 end
