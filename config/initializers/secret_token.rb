1e2fb4c63165776a466ac785e55780636a975439
RainMonster <the.rain.monster@gmail.com>
2014-02-22 20:44:14 -0800

    first commit JRuby project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rouge::Application.config.secret_key_base = 'ee1603a4d7c47136468dfd957a420d20e9d56cf244a4c2f0d360ab3261661320487235bc3cd2b8ce0ad66244ceab402deab29da027623dd6d077217361b70ddf'
