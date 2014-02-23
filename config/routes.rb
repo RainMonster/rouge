41c2b6fd8c937d9ce1f1bc3e0835462bdd72c38e
RainMonster <the.rain.monster@gmail.com>
2014-02-22 21:33:43 -0800

    Devise recommended settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rouge::Application.routes.draw do
+
+  root 'user#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
